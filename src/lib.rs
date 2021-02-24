b1ad9c6eea3975a21521532180bdab4f1b38bce0
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-02-24 20:15:10 +0100

    build: deny additional warnings, forbid unsafe

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
+#![deny(
+    warnings,
+    missing_debug_implementations,
+    rust_2018_idioms,
+    nonstandard_style,
+    future_incompatible,
+    clippy::all
+)]
+#![forbid(unsafe_code)]
+
 pub mod ast;
 pub mod parser;
